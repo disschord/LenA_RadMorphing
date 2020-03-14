783982137e8bcf3cdb17a22d0a51200461e5ed34
Len Anderson <Anderson.Len@outlook.com>
2020-03-14 20:30:55 +0100

    add copyToFomod script

---
+++ copyToFomod.groovy
@@ -0,0 +1,27 @@
+def target = "../../Fallout 4 DEV/LenA_RadMorphingRedux/_base/"
+
+def black = [
+	~/^.+\.code-workspace$/,
+	~/^.+\.groovy$/,
+	~/^.+\.ppj$/
+]
+
+
+
+
+def moveFiles
+moveFiles = { root ->
+	new File(root).eachFile{ f ->
+		if (f.name[0] != '.' && !black.find{f.name ==~ it}) {
+			if (f.file) {
+				new File("${target}${f.parent}").mkdirs()
+				new File("${target}${f.path}") << f.bytes
+			} else {
+				moveFiles(f.path)
+			}
+		}
+	}
+}
+
+new File(target).deleteDir()
+moveFiles('.')
